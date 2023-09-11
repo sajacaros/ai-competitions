327955ed36146085ee4d6e5a9b59b3bb30037ef9
dukim <sajacall82@naver.com>
2023-09-11 22:57:41 +0900

    mission complete

--- dash/tutorial_practice.py
+++ dash/tutorial_practice.py
@@ -11,7 +11,44 @@ app = Dash(__name__, external_stylesheets=external_stylesheets, suppress_callbac
 #     16.99     1.01   Female    No      Sun   Dinner    2
 df = plotly.data.tips()
 
-app.layout = dbc.Container([html.H4('hello world')])
+@callback(
+    Output(component_id='bar_graph', component_property='figure'),
+    Input(component_id='radio_input', component_property='value')
+)
+def select_smoker(is_smoker):
+    return px.histogram(
+        data_frame=df[df['smoker'] == is_smoker],
+        x='day',
+        y='tip',
+        color='sex',
+        barmode='group',
+        histfunc='avg'
+    )
+
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(dbc.Row([
+            dbc.RadioItems(id='radio_input', options={'Yes': 'smoker', 'No': 'non smoker'}, inline=True, value='No')
+        ]), width=2),
+        dbc.Col(dbc.Row([
+            dcc.Graph(id='bar_graph', figure={})
+        ]), width=10),
+    ])
+
+    # seaborn
+    # data, x, y, hue
+    # plotly
+    # data_frame, x, y, color
+    # dcc.Graph(figure=px.histogram(
+    #     data_frame=df,
+    #     x='day',
+    #     y='tip',
+    #     color='sex',
+    #     barmode='group',
+    #     histfunc='avg'
+    # ))
+], style={'backgroundColor':'yellow'}, fluid=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
